45136e573b4e9c5f8e24e5944632c05910f39d26
Coy Sanders <coymeetsworld@gmail.com>
2018-10-02 01:24:51 -0700

    Added code to pass first tests

--- src/App.js
+++ src/App.js
@@ -3,24 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      /* class and id have their own uses in the app. 
+         Developers may come and change it, thus ruining the tests.
+         data-test is a made-up attribute used just for testing.
+      */
+      <div data-test="component-app">
+        <h1 data-test="counter-display">The counter is currently</h1> 
+        <button data-test="increment-button">Increment counter</button>
       </div>
+      
     );
   }
 }
