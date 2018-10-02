1f2510a5ee469e8412521464af9cb72d663b8332
Coy Sanders <coymeetsworld@gmail.com>
2018-10-02 01:06:15 -0700

    DRY refactoring tests

--- src/App.test.js
+++ src/App.test.js
@@ -8,25 +8,48 @@ import App from './App';
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 
+/**
+ * Factory function to create ShallowWrapper for the App component.
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @param {any} state - Initial state for setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props={}, state=null) => {
+  return shallow(<App {...props} />)
+}
+
+
+/**
+ * Return ShallowWraper containing node(s) with the given data-test value. 
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
+ * @param {String} val - Value of data-test attribute for search.
+ * @return {ShallowWrapper}
+ */
+const findByTestAddr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+}
+
+
 /* class and id have their own uses in the app. 
   Developers may come and change it, thus ruining the tests.
   data-test is a made-up attribute used just for testing.
 */
 test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByTestAddr(wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
 });
 
 test('renders increment button', () => {
-  const wrapper = shallow(<App />);
-  const button = wrapper.find("[data-test='increment-button']");
+  const wrapper = setup();
+  const button = findByTestAddr(wrapper, 'increment-button');
   expect(button.length).toBe(1);
 });
 
 test('renders counter display', () => {
-  const wrapper = shallow(<App />);
-  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  const wrapper = setup();
+  const counterDisplay = findByTestAddr(wrapper, 'counter-display');
   expect(counterDisplay.length).toBe(1);
 });
 
