b13f269bd02208b773f572f7a31211e0c02fa504
Coy Sanders <coymeetsworld@gmail.com>
2018-11-27 00:07:13 -0800

    Added Congrats test and component shell

---
+++ jotto/src/Congrats.js
@@ -0,0 +1,17 @@
+/**
+  It will recieve the success prop from its parent, and show a congrats banner if prop exists.
+
+  Simple component, so it doesn't need to connect to Redux or use a Class.
+*/
+import React from 'react';
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @returns {JSX.Element} - Rendered component (or null if 'success' prop doesn't exist)
+ */
+export default () => {
+  return (
+    <div />
+  );
+}
