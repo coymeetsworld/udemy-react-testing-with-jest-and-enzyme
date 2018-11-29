f3a1d1369cdb542da6f3c05720c4ac368384d5fd
Coy Sanders <coymeetsworld@gmail.com>
2018-11-28 22:12:47 -0800

    Added tests when words have been guessed.

--- jotto/src/GuessedWords.test.js
+++ jotto/src/GuessedWords.test.js
@@ -43,4 +43,34 @@ describe('if there are no words guessed', () => {
 
 describe('if there are words guessed', () => {
 
+  let wrapper; //let the scope of wrapper be the whole describe (beforeEach() and test())
+
+  // We'll get more insight in our tests if we have multiple guesses, not just one.
+  const guessedWords = [
+    { guessedWord: 'train', letterMatchCount: 3},
+    { guessedWord: 'agile', letterMatchCount: 1},
+    { guessedWord: 'party', letterMatchCount: 5},
+  ];
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords }); //overriding the default by specifying no guessed words.
+  })
+
+  test ('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+
+  test('renders "guessed words" section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+    expect(guessedWordsNode.length).toBe(1);
+  });
+
+  test('correct number of guessed words', () => {
+    // Not using the word table, and not requiring it to be in one in our tests. 
+    // We're testing the specs are met, not the implementation detail.
+  
+    const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordsNodes.length).toBe(guessedWords.length);
+  });
 });
