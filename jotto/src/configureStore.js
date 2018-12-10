296247e5a556c7f0b466b9966a403c881af6467d
Coy Sanders <coymeetsworld@gmail.com>
2018-12-09 23:09:13 -0800

    Added Redux Thunk to project

--- jotto/src/configureStore.js
+++ jotto/src/configureStore.js
@@ -1,6 +1,10 @@
 // This file will be useful when we start to add middlewares.
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-export default createStore(rootReducer);
+export const middlewares = [ReduxThunk];
+const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+
+export default createStoreWithMiddleware(rootReducer);
 
