ece98d6d6a0d6f5f7360da247f5f5cb640042fde
Coy Sanders <coymeetsworld@gmail.com>
2018-12-11 23:02:40 -0800

    Added code to pass guessing words tests.

--- jotto/src/reducers/guessedWordsReducer.js
+++ jotto/src/reducers/guessedWordsReducer.js
@@ -1,8 +1,7 @@
+import { actionTypes } from '../actions';
 
-/**
- * One reducer per file, that's the convention
- */
 
+//One reducer per file, that's the convention
 
 /**
  * @function guessedWordsReducer
@@ -10,6 +9,11 @@
  * @param {object} action - action to be reduced.
  * @returns {array} - new guessedWords state.
  */
-export default (state, action) => {
-  return null
+export default (state=[], action) => {
+  switch(action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
 }
