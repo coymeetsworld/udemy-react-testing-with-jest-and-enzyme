652cd7e72edeff879f49e6975febc691811d2b72
Coy Sanders <coymeetsworld@gmail.com>
2018-12-17 23:49:37 -0800

    Connect App component (and tests)

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
+import Input from './Input';
+import { getSecretWord } from './actions';
 
 class App extends Component {
   render() {
@@ -11,13 +14,18 @@ class App extends Component {
     return (
       <div className="container">
         <h1>Jotto</h1>
-        <Congrats success={true} /> 
-        <GuessedWords guessedWords={[
-          {guessedWord: 'train', letterMatchCount: 3}
-        ]} />
+        <Congrats success={this.props.success} /> 
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  // get what we need from the state
+  const { success, guessedWords, secretWord } = state;
+  return { success, guessedWords, secretWord };
+}
+
+export default connect(mapStateToProps, { getSecretWord } )(App);
